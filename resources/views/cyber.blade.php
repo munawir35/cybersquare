64c6307ec7c09ea626b70d5c7cc2f2ef9c63eddd
munawir4 <munawir4@gmail.com>
2021-09-29 12:34:19 +0530

    made

--- resources/views/cyber.blade.php
+++ resources/views/cyber.blade.php
@@ -13,31 +13,31 @@
             margin-top: 150px;
             border: 3px;
             border-style: solid;
-            border-color: yellowgreen;
+            border-color: rgb(105, 12, 28);
             border-radius: 5%;
             padding: 20px;
         }
     }
 }
-.aa {
-    color: yellow;
+.kk {
+    color: rgb(105, 12, 28);
 }
     </style>
 </head>
 
 
-<body class="ll" align="center">
+<body class="ll" " align="center ">
 
-    <h1 class="aa"><u> WELCOME CYBERSQUARE </u></h1>
+    <h1 class="kk "><u> WELCOME CYBERSQUARE </u></h1>
     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sunt, harum cum ipsa molestias obcaecati architecto quod tenetur eum a dolores repellat assumenda porro. Adipisci repellendus non quod quasi tempore. Lorem ipsum dolor sit amet, consectetur
         adipisicing elit. Id alias placeat saepe. Velit quasi vitae ducimus tempore labore ad unde sunt exercitationem molestiae. Tempore repudiandae placeat vitae hic, natus quasi.
 
         <br>
         <br>
-        <a href="http://127.0.0.1:8000/about">ABOUT</a>
+        <a href="http://127.0.0.1:8000/about ">ABOUT</a>
         <br>
         <br>
-        <a href="http://127.0.0.1:8000/contact">CONTACT</a>
+        <a href="http://127.0.0.1:8000/contact ">CONTACT</a>
 
     </div>
 
